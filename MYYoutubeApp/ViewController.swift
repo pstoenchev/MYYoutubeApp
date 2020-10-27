c41ba1323fd7c78907d925cf0d87f57b77bfc2e9
Petar Stoenchev <peter.stoenchev@abv.bg>
2020-10-27 17:41:06 +0200

    Initial Commit

---
+++ MYYoutubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYYoutubeApp
+//
+//  Created by Mac on 27.10.20.
+//  Copyright © 2020 Petar Stoenchev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
