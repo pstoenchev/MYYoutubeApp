9c1b349fce738713721c7dca65c24023becf05ff
Petar Stoenchev <peter.stoenchev@abv.bg>
2020-10-30 16:52:19 +0200

    add Response

--- MYYoutubeApp/Response.swift
+++ MYYoutubeApp/Response.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct Response: Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case items
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try
+            decoder.container(keyedBy: CodingKeys.self)
+        items = try container.decode([Video].self, forKey: .items)
+        
+    }
+}
